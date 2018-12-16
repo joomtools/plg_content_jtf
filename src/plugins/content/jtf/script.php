c1f67a2870452943533940173a667109b4a87e23
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2018-12-16 16:28:37 +0100

    Bereinigung nicht mehr benötigter Dateien

--- src/plugins/content/jtf/script.php
+++ src/plugins/content/jtf/script.php
@@ -10,6 +10,11 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Filesystem\File;
+use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Language\Text;
+
 /**
  * Script file of Joomla CMS
  *
@@ -30,32 +35,81 @@ class PlgContentJtfInstallerScript
 	}
 
 	/**
-	 * Function to act prior to installation process begins
+	 * Function to act prior the installation process begins
 	 *
-	 * @param   string     $action    Which action is happening (install|uninstall|discover_install|update)
-	 * @param   JInstaller $installer The class calling this method
+	 * @param   string      $action     Which action is happening (install|uninstall|discover_install|update)
+	 * @param   JInstaller  $installer  The class calling this method
 	 *
-	 * @return  boolean  True on success
-	 * @since   3.0.0
+	 * @return   boolean  True on success
+	 * @since    3.0.0
 	 */
 	public function preflight($action, $installer)
 	{
-		$app = JFactory::getApplication();
-		JFactory::getLanguage()->load('plg_content_jtf', dirname(__FILE__));
+		$app = Factory::getApplication();
+		Factory::getLanguage()->load('plg_content_jtf', dirname(__FILE__));
 
 		if (version_compare(PHP_VERSION, $this->minimumPhp, 'lt'))
 		{
-			$app->enqueueMessage(JText::_('PLG_CONTENT_JTF_MINPHPVERSION'), 'error');
+			$app->enqueueMessage(Text::_('PLG_CONTENT_JTF_MINPHPVERSION'), 'error');
 
 			return false;
 		}
 
 		if (version_compare(JVERSION, $this->minimumJoomla, 'lt'))
 		{
-			$app->enqueueMessage(JText::_('PLG_CONTENT_JTF_MINJVERSION'), 'error');
+			$app->enqueueMessage(Text::_('PLG_CONTENT_JTF_MINJVERSION'), 'error');
 
 			return false;
 		}
+
+		if ($action === 'update')
+		{
+			$pluginPath = JPATH_PLUGINS . '/content/jtf';
+			$deletes    = [];
+
+			$deletes['folder'] = array(
+				// JTF 3.0.0-rc24 -> 3.0.0-rc25
+				$pluginPath . '/layouts/subform/repeatable',
+				$pluginPath . '/libraries/joomla/form/rules',
+				$pluginPath . '/libraries/jtf/Frameworks',
+				// JTF 3.0.0-rc22 -> 3.0.0-rc23
+				$pluginPath . '/assets/js/system',
+			);
+
+			$deletes['file']   = array(
+				// JTF 3.0.0-rc24 -> 3.0.0-rc25
+				$pluginPath . '/layouts/note.bs3.php',
+				$pluginPath . '/libraries/joomla/form/FormField.php',
+				$pluginPath . '/libraries/joomla/form/fields/combo.php',
+				$pluginPath . '/libraries/joomla/form/fields/file.php',
+				$pluginPath . '/libraries/joomla/form/fields/list.php',
+				$pluginPath . '/libraries/joomla/form/fields/note.php',
+				$pluginPath . '/libraries/joomla/form/fields/plz.php',
+				$pluginPath . '/libraries/joomla/form/fields/subform.php',
+				$pluginPath . '/libraries/joomla/form/fields/submit.php',
+			);
+
+			foreach ($deletes as $key => $delete)
+			{
+				if ($key == 'folder')
+				{
+					if (is_dir($delete))
+					{
+						Folder::delete($delete);
+					}
+				}
+
+				if ($key == 'file')
+				{
+					if (is_file($delete))
+					{
+						File::delete($delete);
+					}
+				}
+			}
+		}
+
+
 
 		return true;
 	}
