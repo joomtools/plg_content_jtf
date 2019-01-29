0ac0c2d8c3b36502684d6bc36f339f797cc3244f
Guido De Gobbis <guido@degobbis.de>
2019-01-29 12:52:15 +0100

    Fix E-Mail output
    Fix missing output of subforms, if not multiform
    Fix output of note

---
+++ src/plugins/content/jtf/layouts/jtf/message/html/subform.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package          Joomla.Plugin
+ * @subpackage       Content.Jtf
+ *
+ * @author           Guido De Gobbis <support@joomtools.de>
+ * @copyright    (c) 2018 JoomTools.de - All rights reserved.
+ * @license          GNU General Public License version 3 or later
+ */
+
+defined('_JEXEC') or die;
+
+extract($displayData);
+
+/**
+ * Layout variables
+ * ---------------------
+ * @var   JForm   $form
+ * @var   mixed   $value
+ * @var   string  $fieldName
+ * @var   string  $fileClear
+ * @var   string  fileTimeOut
+ * @var   bool    $fieldMultiple
+ */
+
+if (!empty($value))
+{
+	?>
+	<table cellpadding="2" border="1">
+		<tbody>
+		<?php if ($fieldMultiple)
+		{
+			$counter = count($value) - 1;
+
+			for ($i = 0; $i <= $counter; $i++)
+			{
+				$displayData['i'] = $i;
+
+				echo $this->sublayout('fields', $displayData);
+
+				if ($i < $counter)
+				{
+					?>
+					<tr>
+						<td colspan="2">&nbsp;</td>
+					</tr>
+					<?php
+				}
+			}
+		}
+		else
+		{
+			$displayData['i'] = '';
+
+			echo $this->sublayout('fields', $displayData);
+		} ?>
+		</tbody>
+	</table>
+	<?php
+}
+else
+{
+	echo '--';
+}
---
+++ src/plugins/content/jtf/layouts/jtf/message/html/subform/fields.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @package          Joomla.Plugin
+ * @subpackage       Content.Jtf
+ *
+ * @author           Guido De Gobbis <support@joomtools.de>
+ * @copyright    (c) 2018 JoomTools.de - All rights reserved.
+ * @license          GNU General Public License version 3 or later
+ */
+
+defined('_JEXEC') or die;
+
+extract($displayData);
+
+/**
+ * Layout variables
+ * ---------------------
+ * @var   JForm   $form
+ * @var   mixed   $value
+ * @var   string  $fieldName
+ * @var   string  $fileClear
+ * @var   string  fileTimeOut
+ * @var   bool    $fieldMultiple
+ * @var   string  $i
+ */
+
+$subForm       = $form->getField($fieldName)->loadSubForm();
+$subFormFields = $subForm->getGroup('');
+
+foreach ($subFormFields as $subFormField)
+{
+	$values       = isset($value[$fieldName . $i])
+		? $value[$fieldName . $i]
+		: $value;
+	$subFormType  = $subFormField->getAttribute('type');
+	$subFormLabel = $subFormField->getAttribute('label');
+	$subFormName  = $subFormField->getAttribute('name');
+	$subFormValue = $values[$subFormName];
+
+	if (!empty($subFormField->getAttribute('notmail')))
+	{
+		continue;
+	}
+
+	if (empty($subFormValue))
+	{
+		// Comment out 'continue', if you want to submit only filled fields
+		// continue;
+	}
+
+	if ($subFormType == 'file' && $fileTimeOut == '' && $fileClear > 0)
+	{
+		$fileTimeOut .= '<tr><td colspan="2">';
+		$fileTimeOut .= JText::sprintf('JTF_FILE_TIMEOUT', $fileClear);
+		$fileTimeOut .= '</td></tr>';
+
+		$form->setAttribute('fileTimeOut', $fileTimeOut);
+	}
+
+	if (is_array($subFormValue))
+	{
+		foreach ($subFormValue as $_key => $_value)
+		{
+			if ($subFormType == 'file')
+			{
+				$subFormValues[] = '<a href="' . $_value . '" download>' . $_key . '</a> *';
+			}
+			else
+			{
+				$subFormValues[] = strip_tags(trim(JText::_($_value)));
+			}
+		}
+
+		if (empty($subFormValues))
+		{
+			$subFormValues = array();
+		}
+
+		$subFormValue = implode(", ", $subFormValues);
+		unset ($subFormValues);
+
+	} ?>
+	<tr>
+		<th style="width:30%; text-align: left;">
+			<?php echo strip_tags(JText::_($subFormLabel)); ?>
+		</th>
+		<td><?php echo !empty($subFormValue)
+				? nl2br(JText::_($subFormValue))
+				: '--'; ?>
+		</td>
+	</tr>
+	<?php
+}
+
