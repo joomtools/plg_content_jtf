b8a9bedb029c9a915aa4feab752c9df0ee0b184c
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2019-04-26 17:21:10 +0200

    Add prepare content on parsing option "message_article=articleID"

--- src/plugins/content/jtf/jtf.php
+++ src/plugins/content/jtf/jtf.php
@@ -18,6 +18,7 @@ use Joomla\CMS\Language\Associations;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Profiler\Profiler;
+use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Utilities\ArrayHelper;
 use Jtf\Form\Form;
 
@@ -1449,6 +1450,9 @@ class PlgContentJtf extends CMSPlugin
 			->where('id=' . $db->quote($id));
 
 		$content = $db->setQuery($query)->loadObject();
+
+		// Prepare content
+		$content->text = JHtml::_('content.prepare', $content->text, '', 'mod_custom.content');
 
 		return $content;
 	}
