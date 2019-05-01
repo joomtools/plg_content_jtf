024e26ecd24923328a5b895438a62df7f06ad5b6
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2019-05-01 20:33:38 +0200

    Add clearing of orphaned files and folders on update

--- src/plugins/content/jtf/script.php
+++ src/plugins/content/jtf/script.php
@@ -3,9 +3,9 @@
  * @package      Joomla.Plugin
  * @subpackage   Content.Jtf
  *
- * @author       Guido De Gobbis <support@joomtools.de>
- * @copyright    (c) 2018 JoomTools.de - All rights reserved.
- * @license      GNU General Public License version 3 or later
+ * @author      Guido De Gobbis <support@joomtools.de>
+ * @copyright   2019 JoomTools.de - All rights reserved.
+ * @license     GNU General Public License version 3 or later
  */
 
 defined('_JEXEC') or die;
@@ -18,21 +18,26 @@ use Joomla\CMS\Language\Text;
 /**
  * Script file of Joomla CMS
  *
- * @since   3.0.0
+ * @since  3.0.0
  */
 class PlgContentJtfInstallerScript
 {
 	/**
-	 * Extension script constructor.
+	 * Minimum Joomla version to install
+	 *
+	 * @var   string
 	 *
 	 * @since   3.0.0
 	 */
-	public function __construct()
-	{
-		// Define the minumum versions to be supported.
-		$this->minimumJoomla = '3.9';
-		$this->minimumPhp    = '7.1';
-	}
+	public $minimumJoomla = '3.9';
+	/**
+	 * Minimum PHP version to install
+	 *
+	 * @var   string
+	 *
+	 * @since   3.0.0
+	 */
+	public $minimumPhp = '7.0';
 
 	/**
 	 * Function to act prior the installation process begins
@@ -41,66 +46,66 @@ class PlgContentJtfInstallerScript
 	 * @param   JInstaller  $installer  The class calling this method
 	 *
 	 * @return   boolean  True on success
-	 * @since    3.0.0
+	 * @throws   Exception
+	 *
+	 * @since   3.0.0
 	 */
 	public function preflight($action, $installer)
 	{
-		$app = Factory::getApplication();
+		$notDeleted = '';
+		$app        = Factory::getApplication();
+		$pluginPath = JPATH_PLUGINS . '/content/jtf';
+
 		Factory::getLanguage()->load('plg_content_jtf', dirname(__FILE__));
 
 		if (version_compare(PHP_VERSION, $this->minimumPhp, 'lt'))
 		{
-			$app->enqueueMessage(Text::_('PLG_CONTENT_JTF_MINPHPVERSION'), 'error');
+			$app->enqueueMessage(Text::sprintf('PLG_CONTENT_JTF_MINPHPVERSION', $this->minimumPhp), 'error');
 
 			return false;
 		}
 
 		if (version_compare(JVERSION, $this->minimumJoomla, 'lt'))
 		{
-			$app->enqueueMessage(Text::_('PLG_CONTENT_JTF_MINJVERSION'), 'error');
+			$app->enqueueMessage(Text::sprintf('PLG_CONTENT_JTF_MINJVERSION', $this->minimumJoomla), 'error');
 
 			return false;
 		}
 
 		if ($action === 'update')
 		{
-			$pluginPath = JPATH_PLUGINS . '/content/jtf';
-			$deletes    = [];
+			$deletes = [];
 
 			$deletes['folder'] = array(
-				// JTF 3.0.0-rc24 -> 3.0.0-rc25
-				$pluginPath . '/layouts/joomla/form/field/subform/repeatable',
-				$pluginPath . '/libraries/joomla/form/rules',
-				$pluginPath . '/libraries/jtf/Frameworks',
-				// JTF 3.0.0-rc22 or older -> 3.0.0-rc23
+				// before 3.0.0-rc31
+				$pluginPath . '/assets/fields',
+				$pluginPath . '/assets/frameworks',
+				$pluginPath . '/assets/j3.7.x',
+				$pluginPath . '/assets/j3.8.x',
+				$pluginPath . '/assets/rules',
 				$pluginPath . '/assets/js/system',
 				$pluginPath . '/libraries/frameworks',
 			);
 
-			$deletes['file']   = array(
-				// JTF 3.0.0-rc24 -> 3.0.0-rc25
-				$pluginPath . '/layouts/note.bs3.php',
-				$pluginPath . '/libraries/joomla/form/FormField.php',
-				$pluginPath . '/libraries/joomla/form/fields/combo.php',
-				$pluginPath . '/libraries/joomla/form/fields/file.php',
-				$pluginPath . '/libraries/joomla/form/fields/list.php',
-				$pluginPath . '/libraries/joomla/form/fields/note.php',
-				$pluginPath . '/libraries/joomla/form/fields/plz.php',
-				$pluginPath . '/libraries/joomla/form/fields/subform.php',
-				$pluginPath . '/libraries/joomla/form/fields/submit.php',
+			$deletes['file'] = array(
+				// before 3.0.0-rc31
+				$pluginPath . '/assets/file.php',
+				$pluginPath . '/layouts/joomla/form/renderfield.bs3.php',
+				$pluginPath . '/layouts/joomla/form/renderfield.uikit.php',
+				$pluginPath . '/layouts/joomla/form/renderfield.uikit3.php',
+				$pluginPath . '/layouts/jtf/form.bs3.php',
+				$pluginPath . '/layouts/jtf/form.uikit3.php',
 				JPATH_ROOT . '/administrator/language/de-DE/de-DE.plg_content_jtf.ini',
 				JPATH_ROOT . '/administrator/language/de-DE/de-DE.plg_content_jtf.sys.ini',
 				JPATH_ROOT . '/administrator/language/en-GB/en-GB.plg_content_jtf.ini',
 				JPATH_ROOT . '/administrator/language/en-GB/en-GB.plg_content_jtf.sys.ini',
 			);
 
-			$notDeleted = '';
-
 			foreach ($deletes as $key => $orphans)
 			{
 				$notDeleted .= $this->deleteOrphans($key, $orphans);
 			}
-			
+
 			if (!empty($notDeleted))
 			{
 				$app->enqueueMessage($notDeleted, 'error');
@@ -114,8 +119,9 @@ class PlgContentJtfInstallerScript
 	 * @param   string  $type     Wich type are orphans of (file or folder)
 	 * @param   array   $orphans  Array of files or folders to delete
 	 *
+	 * @return   string
 	 *
-	 * @since version
+	 * @since   3.0.0
 	 */
 	private function deleteOrphans($type, array $orphans)
 	{
@@ -133,7 +139,6 @@ class PlgContentJtfInstallerScript
 					}
 				}
 			}
-
 			if ($type == 'file')
 			{
 				if (is_file($item))
@@ -145,7 +150,7 @@ class PlgContentJtfInstallerScript
 				}
 			}
 		}
-		
+
 		return $notDeleted;
 	}
 }
