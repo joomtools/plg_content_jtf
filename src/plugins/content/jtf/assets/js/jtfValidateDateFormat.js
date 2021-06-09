54fca9879f2a32a9c39068e063ccc6f9911f6b50
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2021-06-09 08:05:16 +0200

    Add possible date format to JS validation for calendar field

--- src/plugins/content/jtf/assets/js/jtfValidateDateFormat.js
+++ src/plugins/content/jtf/assets/js/jtfValidateDateFormat.js
@@ -10,7 +10,7 @@
  */
 document.addEventListener('DOMContentLoaded', function () {
   document.formvalidator.setHandler('dateformat', function (value) {
-    var dayformat = ['L', 'D.M.YY', 'D.M.YY hh:mm:ss', 'D-M-YY', 'D-M-YY hh:mm:ss', 'D/M/YY', 'D/M/YY hh:mm:ss', 'D.M.YYYY', 'D.M.YYYY hh:mm:ss', 'D-M-YYYY', 'D-M-YYYY hh:mm:ss', 'D/M/YYYY', 'D/M/YYYY hh:mm:ss', 'DD.MM.YY', 'DD.MM.YY hh:mm:ss', 'DD-MM-YY', 'DD-MM-YY hh:mm:ss', 'DD/MM/YY', 'DD/MM/YY hh:mm:ss', 'DD.MM.YYYY', 'DD.MM.YYYY hh:mm:ss', 'DD-MM-YYYY', 'DD-MM-YYYY hh:mm:ss', 'DD/MM/YYYY', 'DD/MM/YYYY hh:mm:ss'],
+    var dayformat = ['L', 'D.M.YY', 'D.M.YY hh:mm', 'D.M.YY hh:mm:ss', 'D-M-YY', 'D-M-YY hh:mm', 'D-M-YY hh:mm:ss', 'D/M/YY', 'D/M/YY hh:mm', 'D/M/YY hh:mm:ss', 'D.M.YYYY', 'D.M.YYYY hh:mm', 'D.M.YYYY hh:mm:ss', 'D-M-YYYY', 'D-M-YYYY hh:mm', 'D-M-YYYY hh:mm:ss', 'D/M/YYYY', 'D/M/YYYY hh:mm', 'D/M/YYYY hh:mm:ss', 'DD.MM.YY', 'DD.MM.YY hh:mm', 'DD.MM.YY hh:mm:ss', 'DD-MM-YY', 'DD-MM-YY hh:mm', 'DD-MM-YY hh:mm:ss', 'DD/MM/YY', 'DD/MM/YY hh:mm', 'DD/MM/YY hh:mm:ss', 'DD.MM.YYYY', 'DD.MM.YYYY hh:mm', 'DD.MM.YYYY hh:mm:ss', 'DD-MM-YYYY', 'DD-MM-YYYY hh:mm', 'DD-MM-YYYY hh:mm:ss', 'DD/MM/YYYY', 'DD/MM/YYYY hh:mm', 'DD/MM/YYYY hh:mm:ss'],
         moment = window.moment || {};
     return moment(value, dayformat, 'de', true).isValid();
   });
